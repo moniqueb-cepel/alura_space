99b6d61096063a0403cbd122c998d203badce3c1
moniqueb-cepel <32939067+moniqueb-cepel@users.noreply.github.com>
2025-04-02 11:41:16 -0300

    Configurando o admin do django para mostrar no site: a tabela,campos,links,busca. E incluão do campo categoria.

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+# importa o modelo/classe 
+from galeria.models import Fotografia
+
+# para personalizar o grid de fotografias no site 
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("nome", "legenda", "categoria", "descricao", "foto")
+
+    # por default o link já é no primeiro campo
+    # mas se quiser modificar, ou colocar em varios campos, é só configurar
+    list_display_links = ("nome", "categoria")
+
+    # para colocar campo de busca
+    # lembrar de colocar sempre uma virgula no final, por ser tupla
+    search_fields = ("categoria",)
+
+# registra o modelo 
+admin.site.register(Fotografia, ListandoFotografias)
--- galeria/models.py
+++ galeria/models.py
@@ -3,10 +3,22 @@ from django.db import models
 # Criar classes/ representa uma tabela no bd
 # herdando a biblioteca models 
 class Fotografia(models.Model):
+
+    # categoria é uma lista
+    # o CharField é uma tupla
+    # precisa de virgula no final 
+    opcoes_categoria = [
+        ("NEBULOSA", "Nebulosa"),
+        ("ESTRELA", "Estrela"),
+        ("GALÁXIA", "Galáxia"),
+        ("PALNETA", "Planeta"),
+    ]
+
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=opcoes_categoria, default='')
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
-
+    
     def __str__(self):
         return f"Fotografia [nome={self.nome}]"
